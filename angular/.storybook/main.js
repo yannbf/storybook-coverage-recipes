9fe7d03304715d6ec0479dceb3f787752ba15ff2
Yann Braga <yannbf@gmail.com>
2023-01-20 11:09:29 +0100

    update angular-webpack recipe

--- angular/.storybook/main.js
+++ angular/.storybook/main.js
@@ -1,40 +1,35 @@
-const constants = require('@storybook/addon-coverage/dist/cjs/constants');
-
+const path = require('path');
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-coverage',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-coverage",
-  ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/angular',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
   webpackFinal: async (config) => {
-    const rules = config.module?.rules || [];
-    rules.push(
-      {
-        test: /\.(js|ts)$/,
-        loader: 'istanbul-instrumenter-loader',
-        options: { esModules: true },
-        enforce: 'post',
-        include: require('path').join(__dirname, '..', 'src'),
-        exclude: [
-          /\.(e2e|spec|stories)\.ts$/,
-          /node_modules/,
-          /(ngfactory|ngstyle)\.js/,
-        ],
-      });
+    const rules = config.module?.rules || []
+    rules.push({
+      test: /\.(js|ts)$/,
+      loader: '@jsdevtools/coverage-istanbul-loader',
+      enforce: 'post',
+      include: path.join(process.cwd(), 'src'),
+      exclude: [
+        /\.(e2e|spec|stories)\.ts$/,
+        /node_modules/,
+        /(ngfactory|ngstyle)\.js/,
+        /polyfills.ts/
+      ],
+    })
 
-    config.module = config.module || {};
-    config.module.rules = rules;
+    config.module = config.module || {}
+    config.module.rules = rules
 
-    return config;
-  }
+    return config
+  },
 }
--- angular/README.md
+++ angular/README.md
@@ -2,34 +2,31 @@
 
 ## Setting up coverage
 
-Install `istanbul-instrumenter-loader` and register it as part of Storybook's webpack config:
+Install `@jsdevtools/coverage-istanbul-loader` and register it as part of Storybook's webpack config:
 
 ```js
 // .storybook/main.js
 module.exports = {
   webpackFinal: async (config) => {
-    const rules = config.module?.rules || [];
-    rules.push(
-      {
-        test: /\.(js|ts)$/,
-        loader: 'istanbul-instrumenter-loader',
-        options: { esModules: true },
-        enforce: 'post',
-        include: require('path').join(__dirname, '..', 'src'),
-        exclude: [
-          /\.(e2e|spec|stories)\.ts$/,
-          /node_modules/,
-          /polyfills\.ts/,
-          /preview\.js/,
-          /(ngfactory|ngstyle)\.js/,
-        ],
-      });
+    const rules = config.module?.rules || []
+    rules.push({
+      test: /\.(js|ts)$/,
+      loader: '@jsdevtools/coverage-istanbul-loader',
+      enforce: 'post',
+      include: path.join(process.cwd(), 'src'),
+      exclude: [
+        /\.(e2e|spec|stories)\.ts$/,
+        /node_modules/,
+        /(ngfactory|ngstyle)\.js/,
+        /polyfills.ts/
+      ],
+    })
 
-    config.module = config.module || {};
-    config.module.rules = rules;
+    config.module = config.module || {}
+    config.module.rules = rules
 
-    return config;
-  }
+    return config
+  },
 }
 ```
 
